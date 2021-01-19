0de364413fb1655e606b22c2ab18a302256cc1a8
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-19 11:32:05 -0500

    adding InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames'
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
+
+  let liClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
+  // let imgClass = classNames("interviewers__item-image", {"interviewers__item-image--selected": props.selected})
+
+  return (
+    <li className={liClass} onClick={() => props.setInterviewer(props.name)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+}
