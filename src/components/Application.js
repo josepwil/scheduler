a55f78e996ccb9423c0e839439ea725c942704ac
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-21 11:43:49 -0500

    retrieving interview data

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from './DayList';
 import Appointment from './Appointment'
-import getAppointmentsForDay from '../helpers/selectors'
+import { getAppointmentsForDay, getInterview } from '../helpers/selectors'
 
 
 
@@ -13,23 +13,40 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday", 
     days: [], 
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview)
+    return (
+      <Appointment 
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+      />
+    )
+  })
 
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('api/interviewers')
     ])
       .then(all => {
-        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+        setState(prev => ({
+          ...prev, 
+          days: all[0].data, 
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }))
       })
   }, [])
-
 
   return (
     <main className="layout">
@@ -55,11 +72,7 @@ export default function Application(props) {
         
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => {
-          return (
-            <Appointment key={appointment.id} {...appointment} />
-          )
-        })}
+        {schedule}
        <Appointment key="last" time="5pm" /> 
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export default function getAppointmentsForDay(state, day) {
+const getAppointmentsForDay = function(state, day) {
   const filteredAppointments = state.days.filter(curDay => curDay.name === day)
   
   if (filteredAppointments.length === 0) {
@@ -9,3 +9,20 @@ export default function getAppointmentsForDay(state, day) {
     return state.appointments[appointment]
   })
 }
+
+const getInterview = function(state, interview) {
+  if(!interview) {
+    return null;
+  }
+  const interviewerId = interview.interviewer
+  for (let interviewInfo in state.interviewers) {
+    if(state.interviewers[interviewInfo].id === interviewerId) {
+      return {
+        student: interview.student,
+        interviewer: state.interviewers[interviewInfo]
+      }
+    }
+  }
+}
+
+export { getAppointmentsForDay, getInterview }
