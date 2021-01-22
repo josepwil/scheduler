6bd2e4ce2baf6fff59534d6e8207b5c5bcf4ac70
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-22 17:18:33 -0500

    updating spots remaining when booking/cancelling an interview

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { getAppointmentsForDay } from '../helpers/selectors'
 
 const useApplicationData = function() {
 
@@ -27,7 +28,29 @@ const useApplicationData = function() {
   }, [])
 
   // helper functions
+  function getSpots(state, id) {
+    const daysCopy = [...state.days]
+    const theDay = daysCopy.filter((day) => {
+      return day.appointments.includes(id);
+    })
+    return theDay[0]
+  }
+
+
+
   function bookInterview(id, interview) {
+    const dayData = getSpots(state, id)
+    const spotsRemaining = dayData.spots - 1
+    const dayId = dayData.id
+
+    const day = {
+      ...state.days[dayId - 1],
+      spots: spotsRemaining
+    }
+
+    const days = [...state.days]
+    days.splice(dayId - 1, 1, day)
+    
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -38,11 +61,23 @@ const useApplicationData = function() {
     };
     return axios.put(`/api/appointments/${id}`, { interview })
     .then(res => {
-      setState({...state, appointments})
+      setState(prev => {
+        return {...prev, appointments, days}
+      })
     })
   }
 
   function cancelInterview(id, interview) {
+    const dayData = getSpots(state, id)
+    const spotsRemaining = dayData.spots + 1
+    const dayId = dayData.id
+    const day = {
+      ...state.days[dayId - 1],
+      spots: spotsRemaining
+    }
+    const days = [...state.days]
+    days.splice(dayId - 1, 1, day)
+
     const appointment = {
       ...state.appointments[id],
       interview: null
@@ -53,7 +88,9 @@ const useApplicationData = function() {
     }
     return axios.delete(`/api/appointments/${id}`, { interview })
     .then(res => {
-      setState({...state, appointments})
+      setState(prev => {
+        return {...prev, appointments, days}
+      })
     })
   }
   
