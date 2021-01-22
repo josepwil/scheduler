637e04d020725ddf9b9e56a0f2f4cec8e4b6f1aa
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-22 18:45:11 -0500

    starting reducers

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { getAppointmentsForDay } from '../helpers/selectors'
 
 const useApplicationData = function() {
 
