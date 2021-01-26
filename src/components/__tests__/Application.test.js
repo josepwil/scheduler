39e203f2772f1054ec302e90153992061cbb83f0
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-26 16:02:48 -0500

    finishing integration test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,7 +9,8 @@ import {
   prettyDOM,
   getAllByTestId,
   getByAltText,
-  getByPlaceholderText 
+  getByPlaceholderText,
+  queryByText
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -29,19 +30,31 @@ describe("Application", () => {
   
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container } = render(<Application/>)
+
     await waitForElement(() => getByText(container, "Archie Cohen"))
+
     const appointments = getAllByTestId(container, "appointment")
     const appointment = appointments[0];
+
     // adding a new appointment
     fireEvent.click(getByAltText(appointment, "Add"))
+
     fireEvent.change(getByPlaceholderText(appointment, "Enter Student Name"), {
       target: {value: "Lydia Miller-Jones" }
     })
+
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"))
     fireEvent.click(getByText(appointment, "Save"))
-    
-    console.log(prettyDOM(appointment))
 
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   })
 
 })
